a30d4335e84727d7e9a164ca9c265520247dc7bd
LeoZams <zamekula.oleksii@gmail.com>
2018-12-06 18:05:54 +0200

    Create base resultLessons mapping

--- TimetableMapper/Mappers/LessonsMapper.cs
+++ TimetableMapper/Mappers/LessonsMapper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimetableMapper.Enums;
 using TimetableMapper.FpmModels;
 using TimetableMapper.RozkladModels;
 
@@ -13,7 +14,35 @@ namespace TimetableMapper.Mappers
 		public List<ResultLesson> Map(List<RozkladLesson> firstWeekLessons, List<RozkladLesson> secondWeekLessons)
 		{
 			var result = new List<ResultLesson>();
-
+			for (Day  day = 0; day < Day.Sunday; day++)
+			{
+				var dailyLessons = firstWeekLessons.Where(l => l.Day == day).Union(secondWeekLessons.Where(l => l.Day == day)).ToList();
+				for (LessonNumber lesson = 0; lesson < LessonNumber.Fifth; lesson++)
+				{
+					Lazy<ResultLesson> resultLesson = new Lazy<ResultLesson>();
+					var lessonsByNumber = dailyLessons.Where(l => l.LessonNumber == lesson).ToList();
+					if (lessonsByNumber == null)
+						continue;
+					switch (lessonsByNumber.Count)
+					{
+						case 0:
+							break;
+						case 1:
+							//resultLesson.Value.FirstWeekLesson = lessonsByNumber.First();
+							resultLesson.Value.Flasher = false;
+							
+							break;
+						case 2:
+							//resultLesson.Value.FirstWeekLesson = lessonsByNumber.First();
+							//resultLesson.Value.SecondWeekLesson = lessonsByNumber.Last();
+							resultLesson.Value.Flasher = true;
+							break;
+					}
+					resultLesson.Value.DayOfWeek = day;
+					resultLesson.Value.LessonNumber = lesson;
+					result.Add(resultLesson.Value);
+				}
+			}
 			return result;
 		}
 	}
